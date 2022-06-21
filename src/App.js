42762abca5deadfa85494eb4e4dcd8aa320cad23
akchindo <abdullahichindo16@gmail.com>
2022-06-21 22:06:35 +0100

    sample dark mode

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header/Header"
+import MainContent from './components/MainContent/MainContent';
+import Footer from './components/Footer/Footer';
+import { useState } from 'react';
+
+function App() {
+
+  const [darkMode, setDarkMode] = useState(true) 
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
+
+  return (
+    <div className="App-container">
+      <Header darkMode={darkMode}  toggleDarkMode={toggleDarkMode}   />
+      <MainContent darkMode={darkMode}/>
+
+
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,21 @@
+import React from "react"
+import classes from "../MainContent/MainContent.module.css"
+
+
+
+function MainContent(props) {
+    return (
+      <main className={props.darkMode ? classes.dark : '' }>
+        <h1 className={classes.mainTitle}>Fun facts about React</h1>
+        <ul className={classes.mainFacts}>
+         <li>Was first released in 2013</li>
+         <li>Was originally created by Jordan Walke </li>
+         <li>Has well over 100k stars on Github</li>
+         <li>Is maintained by Facebook</li>
+         <li>Powers thousands of enterprise apps, including mobile apps</li>
+       </ul>
+     </main>
+    )
+  }
+
+  export default MainContent
